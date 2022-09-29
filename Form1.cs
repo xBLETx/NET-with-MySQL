8d2edb0e3da6c6dd63f122eae48ab19f36fb196d
sadus174 <spam174@mail.ru>
2022-09-29 15:26:23 +0500

    1

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace NET_with_MySQL
 {
@@ -15,6 +16,31 @@ namespace NET_with_MySQL
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn;
+
+            // строка подключения к БД
+            string connStr = "server=192.168.25.23;port=3306;user=test;" +
+                "database=test;password=12345;";
+            // создаём объект для подключения к БД
+
+            conn = new MySqlConnection(connStr);
+            
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = $"SELECT name FROM stud WHERE id=1";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            // выполняем запрос и получаем ответ
+            string name = command.ExecuteScalar().ToString();
+            // выводим ответ в TextBox
+            label1.Text = name;
+            // закрываем соединение с БД
+            conn.Close();
         }
     }
 }
